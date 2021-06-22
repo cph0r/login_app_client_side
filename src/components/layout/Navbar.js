f3328e5c69842137a5940f25a252129829d6c5c4
cph0r <chiragphor19@gmail.com>
2021-06-22 16:53:57 +0530

    class name fixed

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -7,23 +7,23 @@ function Navbar() {
   const { loggedIn } = useContext(AuthContext);
 
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
               <Link className="nav-link" to="/">Home</Link>
             </li>
             {loggedIn === false && (
               <>
-                <li class="nav-item"><Link className="nav-link" to="/register">Register</Link></li>
-                <li class="nav-item"><Link className="nav-link" to="/login">Log in</Link></li>
+                <li className="nav-item"><Link className="nav-link" to="/register">Register</Link></li>
+                <li className="nav-item"><Link className="nav-link" to="/login">Log in</Link></li>
 
               </>
             )}
             {loggedIn === true && (
               <>
-                <li class="nav-item"><Link className="nav-link" to="/customer">Customers</Link></li>
+                <li className="nav-item"><Link className="nav-link" to="/customer">Customers</Link></li>
                 <LogOutBtn />
               </>
             )}      
