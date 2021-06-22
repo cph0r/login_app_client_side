163af7beadc5b08aef15c470f1a61fae287affd5
cph0r <chiragphor19@gmail.com>
2021-06-22 17:26:06 +0530

    auth context changecontext values fixeed

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,10 +7,10 @@ function AuthContextProvider(props) {
   const [loggedIn, setLoggedIn] = useState(undefined);
 
   async function getLoggedIn() {
-    // const loggedInRes = await axios.get("http://localhost:5000/auth/loggedIn");
     const loggedInRes = await axios.get(
-      "https://hreokuloginapp.herokuapp.com/auth/loggedIn"
-    );
+      // "https://mern-auth-template-tutorial.herokuapp.com/auth/loggedIn"
+      // );
+    "https://hreokuloginapp.herokuapp.com/auth/loggedIn");
 
     setLoggedIn(loggedInRes.data);
   }
